013f9a3e5ef4789e563fd81692f5efc1a02e0c24
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-10-27 11:59:18 -0500

    Local MAIN: use plugin webpack in HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: '/src/index.js',
@@ -20,4 +21,12 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+       new htmlWebpackPlugin ({
+           inject : true,
+           template: './public/index.html',
+           filename: './index.html',
+       })
+    ]
 };
+
