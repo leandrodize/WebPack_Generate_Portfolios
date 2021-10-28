1d07b11297cfe6b4ac36c942d80d46c31ac18faa
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-10-28 11:58:40 -0500

    Local MAIN: activate mode watch in webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,6 +13,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development', //npx webpack --mode development
+    watch: true, //activate mode watch
     resolve: {
         extensions: ['.js'],
         alias: {
